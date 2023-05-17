6892ef787e9caea4e01a047d87fb24a1bfcd0ce5
EmreYY20 <120115560+EmreYY20@users.noreply.github.com>
2023-05-17 17:04:25 +0200

    add, edit button working

--- app.py
+++ app.py
@@ -111,12 +111,13 @@ class Nurse(db.Model):
         self.address = address
         self.phone_number = phone_number
         
-### Routing ###
-@app.route('/login')
+####===Routing===####
+@app.route('/')
 def login():
     return render_template('login.html')
 
-# Dashboard
+###==Admin==###
+# Dashboard 
 @app.route('/dashboard')
 def index():
     return render_template('dashboard.html')
@@ -128,7 +129,59 @@ def patients():
     patients_count = len(patients)
     return render_template('patients.html', patients=patients, patients_count=patients_count)
 
-# Department
+@app.route('/patients/<pid>/delete', methods=["GET", 'POST'])
+def delete_patient(pid):
+    pid = Patients.query.get(pid)
+    if pid:
+        db.session.delete(pid)
+        db.session.commit()
+        flash('Patient deleted successfully.')
+    return redirect("/patients")
+
+@app.route('/patients/<pid>/edit', methods=['GET', 'POST'])
+def edit_patient(pid):
+    patient = Patients.query.get(pid)
+
+    if request.method == 'POST':
+        # Update the patient's information with the submitted form data
+        patient.forename = request.form['forename']
+        patient.lastname = request.form['lastname']
+        patient.age_sex = request.form['age_sex']
+        patient.date_of_birth = request.form['dob']
+        patient.address = request.form['address']
+        patient.date_admitted = request.form['admitted']
+        patient.date_discharged = request.form['discharged']
+        patient.phone_number = request.form['phone']
+
+        # Update other fields as necessary
+
+        # Save the changes to the database
+        db.session.commit()
+
+        # Redirect the user back to the patients listing page or a specific route
+        return redirect("/patients")
+
+    return render_template('edit_patient.html', patient=patient)
+
+@app.route('/add_patient', methods=['POST'])
+def add_patient():
+    forename = request.form['forename']
+    lastname = request.form['lastname']
+    age_sex = request.form['age_sex']
+    date_of_birth = request.form['date_of_birth']
+    address = request.form['address']
+    date_admitted = request.form['date_admitted']
+    date_discharged = request.form['date_discharged']
+    phone_number = request.form['phone_number']
+    # Get other patient attributes...
+
+    new_patient = Patients(forename=forename, lastname=lastname, age_sex=age_sex, date_of_birth=date_of_birth, address=address, phone_number=phone_number, date_admitted=date_admitted, date_discharged=date_discharged)
+    db.session.add(new_patient)
+    db.session.commit()
+
+    return redirect('/patients')
+
+### Department ###
 @app.route('/departments')
 def departments():
     deps = Department.query.all()
@@ -149,28 +202,121 @@ def medicine():
     med = Medicince.query.all()
     return render_template('medicine.html', med=med)
 
-# Doctor
+### Doctor ###
 @app.route('/doctors')
 def doctors():
-    docs = Doctors.query.all()
-    return render_template('doctors.html', docs=docs)
+    doctors = Doctors.query.all()
+    return render_template('doctors.html', doctors=doctors)
 
-@app.route('/doctor/dashboard')
-def doctor():
-    docs = Doctors.query.all()
-    return render_template('/doctor/dashboard.html')
+@app.route('/doctors/<did>/delete', methods=["GET", 'POST'])
+def delete_doctor(did):
+    did = Doctors.query.get(did)
+    if did:
+        db.session.delete(did)
+        db.session.commit()
+        flash('Doctor deleted successfully.')
+    return redirect("/doctors")
 
-# Nurse
+@app.route('/doctors/<did>/edit', methods=['GET', 'POST'])
+def edit_doctor(did):
+    doctors = Doctors.query.get(did)
+
+    if request.method == 'POST':
+        # Update the patient's information with the submitted form data
+        doctors.forename = request.form['forename']
+        doctors.lastname = request.form['lastname']
+        doctors.date_of_birth = request.form['dob']
+        doctors.address = request.form['address']
+        doctors.phone_number = request.form['phone']
+        doctors.dep = request.form['dep']
+
+        # Save the changes to the database
+        db.session.commit()
+
+        # Redirect the user back to the patients listing page or a specific route
+        return redirect("/doctors")
+
+    return render_template('edit_doctors.html', doctors=doctors)
+
+@app.route('/add_doctor', methods=['POST'])
+def add_doctor():
+    forename = request.form['forename']
+    lastname = request.form['lastname']
+    date_of_birth = request.form['date_of_birth']
+    address = request.form['address']
+    phone_number = request.form['phone_number']
+    department = request.form['dep']
+
+    new_doc = Doctors(forename=forename, lastname=lastname, date_of_birth=date_of_birth, address=address, phone_number=phone_number, department=department)
+    db.session.add(new_doc)
+    db.session.commit()
+
+    return redirect('/doctors')
+
+### Nurse ###
 @app.route('/nurse')
 def nurse():
     nurse = Nurse.query.all()
     return render_template('nurse.html', nurse=nurse)
 
-# Rooms
+@app.route('/nurse/<nid>/delete', methods=["GET", 'POST'])
+def delete_nurse(nid):
+    nid = Nurse.query.get(nid)
+    if nid:
+        db.session.delete(nid)
+        db.session.commit()
+        flash('Nurse deleted successfully.')
+    return redirect("/nurse")
+
+@app.route('/nurse/<nid>/edit', methods=['GET', 'POST'])
+def edit_nurse(nid):
+    nurse = Nurse.query.get(nid)
+
+    if request.method == 'POST':
+        # Update the patient's information with the submitted form data
+        nurse.forename = request.form['forename']
+        nurse.lastname = request.form['lastname']
+        nurse.date_of_birth = request.form['dob']
+        nurse.address = request.form['address']
+        nurse.phone_number = request.form['phone']
+
+        # Save the changes to the database
+        db.session.commit()
+
+        # Redirect the user back to the patients listing page or a specific route
+        return redirect("/nurse")
+
+@app.route('/nurse', methods=['POST'])
+def add_nurse():
+    forename = request.form['forename']
+    lastname = request.form['lastname']
+    date_of_birth = request.form['date_of_birth']
+    address = request.form['address']
+    phone_number = request.form['phone_number']
+
+    new_nurse = Doctors(forename=forename, lastname=lastname, date_of_birth=date_of_birth, address=address, phone_number=phone_number)
+    db.session.add(new_nurse)
+    db.session.commit()
+
+    return redirect('/nurse')
+
+### Rooms ###
 @app.route('/rooms')
 def rooms():
     rooms = Room.query.all()
     return render_template('rooms.html', rooms=rooms)
+
+
+
+
+
+###==Doctor==###
+### Doctor private
+@app.route('/doctor/dashboard')
+def doctor():
+    docs = Doctors.query.all()
+    return render_template('/doctor/dashboard.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
